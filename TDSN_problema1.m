7b63b259dd1beda88d55ae78ae52116080af6961
Javier Sanchez Fernandez <sanchezfdez.javier@gmail.com>
2020-01-10 01:14:35 +0100

    Some progress in bin2txt

--- TDSN_problema1.m
+++ TDSN_problema1.m
@@ -26,12 +26,31 @@ T = 0;
 
 [S, F, T] = spectrogram(incognita, window, noverlap, nfft, fs);
 
-% Procesamiento de datos
+% Obtenemos la ristra de bits
 
 bits = zeros(1, 9600);
 
 for i= 1:9600
-    if(max(abs(S(:,i))) > 13.272)
+    if(max(abs(S(:,i))) < 13.272) % 13.2732 = '0'y 13.2711 = '1'
         bits(i) = 1;
     end
-end 
+end
+
+
+% Funcion bit2txt
+
+char_bin = char(bits + '0');
+char(bin2dec(char_bin(1:8))); % Output: 'E'
+
+char_bin_reshaped = reshape(char_bin, 1200, 8);
+
+final_string = '';
+
+%reshape experimental
+
+for j = 1:1200
+    final_string(1,i) = char(bin2dec(char_bin_reshaped(j,:)));
+end
+
+    
+    
