fa1ea3aa6e2bf210fb92a252ae76277153563b9f
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2020-01-16 11:58:17 +0100

    Finished

--- TDSN_problema1.m
+++ TDSN_problema1.m
@@ -1,11 +1,11 @@
-% Demodulación de una señal FSK binaria mediante la STFT
+% 1. DEMODULACION SEÑAL FSK BINARIA
 
-% Preparacion del entorno de trabajo
+% 1.1. Preparacion del entorno de trabajo
 close all
 clear
 load incognita
 
-% Espectrograma
+% 1.2. Espectrograma
 signal = incognita;
 window = 50;
 noverlap = 0;
@@ -18,13 +18,13 @@ xlabel('Frecuency [Hz]');
 ylabel('Time [s]');
 title('Espectrogram');
 
-% Recogida de valores
+% 1.2. Recogida de valores
 
 S = 0; F = 0; T = 0;
 
 [S, F, T] = spectrogram(incognita, window, noverlap, nfft, fs);
 
-% Obtenemos la ristra de bits
+% 1.3. Obtenemos la ristra de bits
 
 bits = zeros(1, 9600);
 
@@ -34,86 +34,46 @@ for i= 1:9600
     end
 end
 
-% Sacamos el mensaje decodificado por consola
-binToTxt(bits)
+% 1.4. Imprimimos el mensaje por consola
+message = binToTxt(bits);
+message
 
 
-% DECODIFICACION DE LA SEÑAL DTMF
+% 2. DECODIFICACION SEÑAL DTMF
 
 % Preparamos el entorno
 clear
-load telef1.mat
+load telef2.mat
 
-% Definicion de constantes
+% 2.1. Definicion de constantes
 f1_freqs = [704, 792, 872, 956];
 f2_freqs = [1224, 1386, 1512];
 window_DTMF = 400;
 noverlap = 0;
 nffs = window_DTMF.*3;
 
-% Representación del espectrograma
+% 2.2. Representación del espectrograma
 figure(2);
 spectrogram(telef, window_DTMF, noverlap, nffs, fs);
 
-% Calculo de la STFT
-
+% 2.3. Calculo de la STFT
 S = 0; F = 0; T = 0;
-
 [S, F, T] = spectrogram(telef, window_DTMF, noverlap, nffs, fs);
 
-% Decodificacion de la señal DTMF
-
-S_set = unique(max(abs(S))); 
-
-% Mapa relacional 
-keySet = {f1_freqs(1) + f2_freqs(1),...
-    f1_freqs(1) + f2_freqs(2),...
-    f1_freqs(1) + f2_freqs(3),...
-    f1_freqs(2) + f2_freqs(1),...
-    f1_freqs(2) + f2_freqs(2),...
-    f1_freqs(2) + f2_freqs(3),...
-    f1_freqs(3) + f2_freqs(1),...
-    f1_freqs(3) + f2_freqs(2),...
-    f1_freqs(3) + f2_freqs(3),...
-    f1_freqs(4) + f2_freqs(1),...
-    f1_freqs(4) + f2_freqs(2),...
-    f1_freqs(4) + f2_freqs(3)}
-
-valueSet = {'1', '2', '3', '4', '5',...
-    '6', '7','8','9','#','0','*'}
-
-dict = containers.Map(keySet, valueSet);
-
-% Bucle de muestreo
+% 2.4. Muestreo
 sampled_S = zeros(601,1);
 real_S = abs(S);
-n = 2
+n = 2;           % vector donde iniciamos el muestreo
 while n <= 42
     sampled_S = [sampled_S real_S(:,n)];
-    n = n+5;
+    n = n+5;    % muestreamos cada 5 posiciones
 end
 
-% Quito el primer vector, residuo de la inizializacion
-sampled_S = sampled_S(:, 2:end);
+sampled_S = sampled_S(:, 2:end); % elimino la primera columna de ceros, residuo de la inicilización.
 
 
-S_posible_repeated = [];
-f1s = [];
-f2s = [];
-for i = 1:9
-    sorted = sort(sampled_S(:,i));
-    disp(sorted(end-1:end));
-    s1 = sorted(end);
-    s2 = sorted(end-1);
-    s1_s2 = [s1 s2];
-    f1s = [f1s s1];
-    f2s = [f2s s2];
-    
-    S_posible_repeated = [S_posible_repeated s1_s2];
-end
-
-set_S_posible = unique(S_posible_repeated);
-
+% 2.5. Creamos dos matrices que contienen las frecuencias f1 y f2 presentes
+% en cada muestra
 
 auxf1= [];
 auxf2=[];
@@ -131,8 +91,41 @@ for j = 1:9
     auxf2 = [auxf2 ; [f21j f22j f23j]];
 end
 
-auxf1
-auxf2
+% 2.6. Decodificamos las frecuencias presentes en cada muestra segun la tabla
+telef = [''];
+for m = 1:9
+    f1m = auxf1(m,:);
+    f2m = auxf2(m,:);
+    
+    if ((f1m(1) == 1) & (f2m(1)==1))
+        telef = [telef '1'];
+    elseif ((f1m(1) == 1) & (f2m(2)==1))
+        telef = [telef '2'];
+    elseif ((f1m(1) == 1) & (f2m(3)==1))
+        telef = [telef '3'];
+    elseif ((f1m(2) == 1) & (f2m(1)==1))
+        telef = [telef '4'];
+    elseif ((f1m(2) == 1) & (f2m(2)==1))
+        telef = [telef '5'];
+    elseif ((f1m(2) == 1) & (f2m(3)==1))
+        telef = [telef '6'];
+    elseif ((f1m(3) == 1) & (f2m(1)==1))
+        telef = [telef '7'];
+    elseif ((f1m(3) == 1) & (f2m(2)==1))
+        telef = [telef '8'];
+    elseif ((f1m(3) == 1) & (f2m(3)==1))
+        telef = [telef '9'];
+    elseif ((f1m(4) == 1) & (f2m(1)==1))
+        telef = [telef '#'];
+    elseif ((f1m(4) == 1) & (f2m(2)==1))
+        telef = [telef '0'];
+    elseif ((f1m(4) == 1) & (f2m(3)==1))
+        telef = [telef '*'];
+    end
+end
+
+% 2.7. Imprimo por consola el número de teléfono obtenido
+telephone = telef
 
 
 
